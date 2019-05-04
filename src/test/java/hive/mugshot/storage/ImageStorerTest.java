9ec4d99b42352147ece4d113cef47534ed8698f3
germano <germanosp11@hotmail.com>
2019-05-04 17:50:58 -0300

    Updated userId as string.

--- src/test/java/hive/mugshot/storage/ImageStorerTest.java
+++ src/test/java/hive/mugshot/storage/ImageStorerTest.java
@@ -1,4 +1,6 @@
 package hive.mugshot.storage;
+import hive.mugshot.exception.ImageNotFoundException;
+import org.apache.commons.lang.RandomStringUtils;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,9 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.Assert.*;
 
@@ -23,7 +25,7 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class ImageStorerTest {
 
-  private Integer userId;
+  private final String userId= RandomStringUtils.randomAlphabetic(8);
   private MockMultipartFile multipartFile;
   private BufferedImage initialImage;
 
@@ -39,7 +41,6 @@ public class ImageStorerTest {
 
   @Before
   public void setUp() throws IOException {
-    userId= ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
     initialImage=new BufferedImage(512,512,BufferedImage.TYPE_INT_RGB);
     var byteArrayOutputStream = new ByteArrayOutputStream();
     ImageIO.write(initialImage,"jpg",byteArrayOutputStream);
@@ -53,44 +54,53 @@ public class ImageStorerTest {
 
   @Test
   public void uploadImage_WhenMultipartFileIsProvided_expectFileExists(){
-    imageStorer.storeImageProfile(userId.toString(),multipartFile,imageName);
-    Path path=Paths.get(rootDir,userId.toString(),imageName);
+    imageStorer.storeImageProfile(userId,multipartFile,imageName);
+    Path path=Paths.get(rootDir,userId,imageName);
     assertTrue(Files.exists(path));
   }
 
   @Test
   public void uploadImage_WhenBufferedImageIsProvided_expectFileExists(){
-    imageStorer.storeImageProfile(userId.toString(),initialImage,imageName);
-    Path path=Paths.get(rootDir,userId.toString(),imageName);
+    imageStorer.storeImageProfile(userId,initialImage,imageName);
+    Path path=Paths.get(rootDir,userId,imageName);
     assertTrue(Files.exists(path));
   }
 
   @Test
   public void deleteImage_WhenImageNameAndUserDirectoryIsNotNull_expectFileDoesNotExists(){
-    imageStorer.deleteImage(userId.toString(),imageName);
-    Path path=Paths.get(rootDir,userId.toString(),imageName);
+    imageStorer.deleteImage(userId,imageName);
+    Path path=Paths.get(rootDir,userId,imageName);
     assertFalse(Files.exists(path));
   }
 
   @Test
   public void loadImage_WhenMultipartIsNotNull_expectConfiguredImageName() throws IOException {
-    imageStorer.storeImageProfile(userId.toString(),multipartFile,imageName);
-    var resource=imageStorer.loadImage(userId.toString(),imageName);
+    imageStorer.storeImageProfile(userId,multipartFile,imageName);
+    var resource=imageStorer.loadImage(userId,imageName);
     assertEquals(imageName,resource.getFile().getName());
   }
 
   @Test
   public void loadImage_WhenMultipartIsNotNull_expectConfiguredImageSize() throws IOException {
-    imageStorer.storeImageProfile(userId.toString(),multipartFile,imageName);
-    var resource=imageStorer.loadImage(userId.toString(),imageName);
+    imageStorer.storeImageProfile(userId,multipartFile,imageName);
+    var resource=imageStorer.loadImage(userId,imageName);
     var image=ImageIO.read(resource.getFile());
     assertEquals(imageSizeInPixels,image.getHeight());
     assertEquals(imageSizeInPixels,image.getWidth());
   }
-
+  @Test(expected = ImageNotFoundException.class)
+  public void loadImage_WhenFileNotExists_expectImageNotFoundException() {
+    imageStorer.loadImage(userId,imageName);
+  }
+  @Test(expected = InvalidPathException.class)
+  public void loadImage_WhenUserDirectoryNameContainsInvalidCharacter_expectInvalidPathException() {
+    var invalidCharacters="></\\^\"*?".toCharArray();
+    var oneInvalidCharacter= RandomStringUtils.random(1,invalidCharacters);
+    imageStorer.loadImage(userId+oneInvalidCharacter,imageName);
+  }
   @After
   public void deleteCreatedDirectoryAndFile() throws IOException {
-    Path createdDirectoryPath=Paths.get(rootDir,userId.toString());
+    Path createdDirectoryPath=Paths.get(rootDir,userId);
     Path createdImagePath=createdDirectoryPath.resolve(imageName);
     Files.deleteIfExists(createdImagePath);
     Files.deleteIfExists(createdDirectoryPath);
