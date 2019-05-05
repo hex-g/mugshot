0a3b7a853e265a2247a0f3a127c170aef17764f8
germano <germanosp11@hotmail.com>
2019-05-04 23:40:06 -0300

    Updated invalid characters for test

--- src/test/java/hive/mugshot/storage/ImageStorerTest.java
+++ src/test/java/hive/mugshot/storage/ImageStorerTest.java
@@ -42,7 +42,7 @@ public class ImageStorerTest {
   @Before
   public void setUp() throws IOException {
     initialImage=new BufferedImage(512,512,BufferedImage.TYPE_INT_RGB);
-    var byteArrayOutputStream = new ByteArrayOutputStream();
+    final var byteArrayOutputStream = new ByteArrayOutputStream();
     ImageIO.write(initialImage,"jpg",byteArrayOutputStream);
     multipartFile=new MockMultipartFile(
         "image",
@@ -55,36 +55,36 @@ public class ImageStorerTest {
   @Test
   public void uploadImage_WhenMultipartFileIsProvided_expectFileExists(){
     imageStorer.storeImageProfile(userId,multipartFile,imageName);
-    Path path=Paths.get(rootDir,userId,imageName);
-    assertTrue(Files.exists(path));
+    Path pathOfStoredImage=Paths.get(rootDir,userId,imageName);
+    assertTrue(Files.exists(pathOfStoredImage));
   }
 
   @Test
   public void uploadImage_WhenBufferedImageIsProvided_expectFileExists(){
     imageStorer.storeImageProfile(userId,initialImage,imageName);
-    Path path=Paths.get(rootDir,userId,imageName);
-    assertTrue(Files.exists(path));
+    Path pathOfStoredImage=Paths.get(rootDir,userId,imageName);
+    assertTrue(Files.exists(pathOfStoredImage));
   }
 
   @Test
   public void deleteImage_WhenImageNameAndUserDirectoryIsNotNull_expectFileDoesNotExists(){
     imageStorer.deleteImage(userId,imageName);
-    Path path=Paths.get(rootDir,userId,imageName);
-    assertFalse(Files.exists(path));
+    Path pathOfDeletedImage=Paths.get(rootDir,userId,imageName);
+    assertFalse(Files.exists(pathOfDeletedImage));
   }
 
   @Test
   public void loadImage_WhenMultipartIsNotNull_expectConfiguredImageName() throws IOException {
     imageStorer.storeImageProfile(userId,multipartFile,imageName);
-    var resource=imageStorer.loadImage(userId,imageName);
+    final var resource=imageStorer.loadImage(userId,imageName);
     assertEquals(imageName,resource.getFile().getName());
   }
 
   @Test
   public void loadImage_WhenMultipartIsNotNull_expectConfiguredImageSize() throws IOException {
     imageStorer.storeImageProfile(userId,multipartFile,imageName);
-    var resource=imageStorer.loadImage(userId,imageName);
-    var image=ImageIO.read(resource.getFile());
+    final var resource=imageStorer.loadImage(userId,imageName);
+    final var image=ImageIO.read(resource.getFile());
     assertEquals(imageSizeInPixels,image.getHeight());
     assertEquals(imageSizeInPixels,image.getWidth());
   }
@@ -94,8 +94,8 @@ public class ImageStorerTest {
   }
   @Test(expected = InvalidPathException.class)
   public void loadImage_WhenUserDirectoryNameContainsInvalidCharacter_expectInvalidPathException() {
-    var invalidCharacters="></\\^\"*?".toCharArray();
-    var oneInvalidCharacter= RandomStringUtils.random(1,invalidCharacters);
+    final var invalidCharacters="><^'*?".toCharArray();
+    final var oneInvalidCharacter= RandomStringUtils.random(1,invalidCharacters);
     imageStorer.loadImage(userId+oneInvalidCharacter,imageName);
   }
   @After
