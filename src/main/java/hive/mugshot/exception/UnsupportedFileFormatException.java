ec593d83314c00591845bec9c1cf98cac00adc66
germano <germanosp11@hotmail.com>
2019-05-05 14:50:04 -0300

    Refactored exception name

--- src/main/java/hive/mugshot/exception/UnsupportedFileFormatException.java
+++ src/main/java/hive/mugshot/exception/UnsupportedFileFormatException.java
@@ -3,5 +3,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.UNSUPPORTED_MEDIA_TYPE, reason = "Media type unsupported")
-public class NotAcceptedFileFormatException extends RuntimeException{
+public class UnsupportedFileFormatException extends RuntimeException{
 }
