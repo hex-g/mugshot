af4cf39fc78b576ecf8e559ec58ba872cfc18693
germano <germanosp11@hotmail.com>
2019-05-05 15:02:54 -0300

    Removed @EntityScan

--- src/main/java/hive/mugshot/MugshotApplication.java
+++ src/main/java/hive/mugshot/MugshotApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EntityScan( basePackages = {"hive.entity"} )
 public class MugshotApplication {
   public static void main(String[] args) {
     SpringApplication.run(MugshotApplication.class, args);
